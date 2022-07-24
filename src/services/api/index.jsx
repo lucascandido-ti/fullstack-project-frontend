9543ea77287d4b45cc76aa0224ac7ce1e4278291
lucas-candido <sistema2@innovationbrindes.com.br>
2022-07-23 21:24:14 -0300

    Corrigindo link da API

--- src/services/api/index.jsx
+++ src/services/api/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const TimeSeries = axios.create({
-    baseURL: `http://localhost:5000/time-series`,
+    baseURL: `http://localhost:5000/data-series`,
     headers: {
       "Content-type": "application/json"
     }
